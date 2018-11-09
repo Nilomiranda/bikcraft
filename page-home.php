461ffd176aa35419e290ffed35479aa09c861827
Danilo Miranda <danilomiranda@Danilos-MacBook-Pro.local>
2018-11-08 22:36:11 -0200

    Added some custom fields to homepage (page-home.php) and split the code into different files

--- page-home.php
+++ page-home.php
@@ -7,10 +7,10 @@
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<section class="introducao">
 			<div class="container">
-				<h1>Bicicletas Feitas a Mão</h1>
+				<h1><?php the_field('home_title') ?></h1>
 				<blockquote class="quote-externo">
-					<p>“não tenha nada em sua casa que você não considere útil ou acredita ser bonito”</p>
-					<cite>WILLIAM MORRIS</cite>
+					<p><?php the_field('home_quote') ?></p>
+					<cite><?php the_field('home_quote_author') ?></cite>
 				</blockquote>
 				<a href="/products" class="btn">Orçamento</a>
 			</div>
@@ -22,7 +22,7 @@
 
 				<li class="grid-1-3">
 					<div class="produtos_icone">
-						<img src="img/produtos/passeio.png" alt="Bikcraft Passeio">
+						<img src="<?php echo get_template_directory_uri() ?>/img/produtos/passeio.png" alt="Bikcraft Passeio">
 					</div>
 					<h3>Passeio</h3>
 					<p>Muito melhor do que passear pela orla a vidros fechados.</p>
@@ -30,7 +30,7 @@
 
 				<li class="grid-1-3">
 					<div class="produtos_icone">
-						<img src="img/produtos/esporte.png" alt="Bikcraft Esporte">
+						<img src="<?php echo get_template_directory_uri() ?>/img/produtos/esporte.png" alt="Bikcraft Esporte">
 					</div>
 					<h3>Esporte</h3>
 					<p>Mais rápida do que Forrest Gump, ninguém vai pegar você.</p>
@@ -38,7 +38,7 @@
 
 				<li class="grid-1-3">
 					<div class="produtos_icone">
-						<img src="img/produtos/retro.png" alt="Bikcraft Retrô">
+						<img src="<?php echo get_template_directory_uri() ?>/img/produtos/retro.png" alt="Bikcraft Retrô">
 					</div>
 					<h3>Retrô</h3>
 					<p>O passado volta para lembrarmos o que devemos fazer no futuro.</p>
@@ -54,48 +54,14 @@
 		</section>
 		<!-- Fecha Produtos -->
 
-		<section class="portfolio">
-			<div class="container">
-				<h2 class="subtitulo">Portfólio</h2>
-				<div class="portfolio_lista">
-					<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-					<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-					<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-				</div>
-				<div class="call">
-					<p>conheça mais o nosso portfólio</p>
-					<a href="portfolio.html" class="btn">Portfólio</a>
-				</div>
-			</div>
-		</section>
+		<?php include(TEMPLATEPATH . '/partials/part-product.php') ?>
 
-		<section class="qualidade container">
-			<h2 class="subtitulo">Qualidade</h2>
-			<img src="img/bikcraft-qualidade.png" alt="Bikcraft">
-			<ul class="qualidade_lista">
-				<li class="grid-1-3">
-					<h3>Durabilidade</h3>
-					<p>Sólida como pedra, leve como o vento e resistente como o diamante, são nossos diferenciais.</p>
-				</li>
-				<li class="grid-1-3">
-					<h3>Design</h3>
-					<p>Feitas sob medida para o melhor conforto e eficiência. Adaptamos a sua Bikcraft para o seu corpo.</p>
-				</li>
-				<li class="grid-1-3">
-					<h3>Sustentabilidade</h3>
-					<p>Além de ajudar a cuidar do meio ambiente, tirando carros da rua, toda a produção é sustentável.</p>
-				</li>
-			</ul>
-			<div class="call">
-				<p>conheça mais a nossa história</p>
-				<a href="sobre.html" class="btn btn-preto">Sobre</a>
-			</div>
-		</section>
+		<?php include(TEMPLATEPATH . '/partials/part-about.php') ?>
 
 		<div class="quebra">
 			<blockquote class="quote-externo container">
-				<p>“o verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana.”</p>
-				<cite>WILLIAM MORRIS</cite>
+				<p><?php the_field('home_end_quote') ?></p>
+				<cite><?php the_field('home_end_quote_author') ?></cite>
 			</blockquote>
 		</div>
 	<?php endwhile; endif ?>
---
+++ partials/part-product.php
@@ -0,0 +1,16 @@
+<!-- PORTFOLIO PARTIAL -->
+
+<section class="portfolio">
+    <div class="container">
+        <h2 class="subtitulo">Portfólio</h2>
+        <div class="portfolio_lista">
+            <div class="grid-8"><img src="<?php echo get_template_directory_uri() ?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
+            <div class="grid-8"><img src="<?php echo get_template_directory_uri() ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
+            <div class="grid-16"><img src="<?php echo get_template_directory_uri() ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
+        </div>
+        <div class="call">
+            <p>conheça mais o nosso portfólio</p>
+            <a href="portfolio.html" class="btn">Portfólio</a>
+        </div>
+    </div>
+</section>
