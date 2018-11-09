af2a745f042f6df0c0d29a3349d710ce974f28f2
Danilo Miranda <danilomiranda@Danilos-MacBook-Pro.local>
2018-11-08 23:31:31 -0200

    Link fix

--- partials/part-product.php
+++ partials/part-product.php
@@ -8,9 +8,11 @@
             <div class="grid-8"><img src="<?php echo get_template_directory_uri() ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
             <div class="grid-16"><img src="<?php echo get_template_directory_uri() ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
         </div>
+        <?php if ( !is_page('Portfolio') ) { ?>
         <div class="call">
             <p>conheça mais o nosso portfólio</p>
-            <a href="portfolio.html" class="btn">Portfólio</a>
+            <a href="/portfolio" class="btn">Portfólio</a>
         </div>
+        <?php } ?>
     </div>
 </section>
